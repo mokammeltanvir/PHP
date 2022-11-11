b130abe2479d4a116cc69900e52268c11f422077
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2022-11-12 02:04:09 +0600

    php in htmlpage form and file upload

---
+++ Mastering-in-PHP-LWHH/4-HTML webPage/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Webpage</title>
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+        <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+        <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
+        <style>
+            body{
+                margin-top:30px;
+            }
+        </style>
+</head>
+<body>
+  <div class="container">
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <h1>Welcome</h1>
+                    <h2>
+                        <?php echo "Hello World"; ?>
+                    </h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ad exercitationem libero numquam tenetur minus veniam suscipit dicta asperiores fuga!</p>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
